6c24a273163d51c654e8864876b5d580fe2d9022
exhertz <cactuseveryday@tuta.io>
2024-12-17 22:20:47 +0300

    refactor(core): ded. fn to check env variables

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,17 +4,19 @@ import {
     Partials,
     Guild
 } from 'discord.js';
+import logger from './modules/logger.js';
 
 import 'dotenv/config';
 
-import logger from './modules/logger.js';
-
 const startTime = Date.now();
-const requiredEnvVariables = [
-    'TOKEN'
-];
 
-const client = new Client({
+if (hasMissingEnvVariables([
+    'TOKEN'
+])) {
+    process.exit(1);
+}
+
+export const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
@@ -36,17 +38,8 @@ const client = new Client({
     ]
 });
 
-Object.assign(client, logger);
 client.config = process.env;
-
-const missingEnvVariables = requiredEnvVariables.filter((varName) => !process.env[varName]);
-
-if (missingEnvVariables.length > 0) {
-    console.error(`Missing .env variables: ${missingEnvVariables.join(', ')}`);
-    process.exit(1);
-}
-
-client.login(client.config.TOKEN);
+Object.assign(client, logger);
 
 client.on('ready', async () => {
     client.log('core', `Application started -> ${Date.now()-startTime} ms.`);
@@ -58,13 +51,34 @@ client.on('ready', async () => {
     client.log('core', serverList);
 
     setInterval(() => {
-        const newDate = new Date();
-        const dateStr = newDate.toLocaleDateString().slice(0, -5);
-        const timeStr = newDate.toLocaleTimeString();
-        const l = `[~] ${dateStr} ${timeStr} | PING: ${Math.round(global.client.ws.ping)} ms`;
-        client.log('core', l);
-    }, 1000 * 60 * 60);
+        client.log('core', `WebSocket ping: ${Math.round(client.ws.ping)} ms`);
+    }, 1000 * 60 * 20);
 });
+
+const startup = async () => {
+    client.log('core', '');
+    try {
+        await import('@Plugins/index.js');
+        client.log('core', ':: Plugins Loaded');
+    } catch (err) {
+        console.error(err);
+        client.warn('core', 'Failed to load any plugins, a plugin has errors in it.');
+    }
+
+    client.login(client.config.TOKEN);
+};
+
+startup();
+
+function hasMissingEnvVariables(variables: string[]): boolean {
+    const missingEnvVariables = variables.filter((varName) => !process.env[varName]);
+
+    if (missingEnvVariables.length > 0) {
+        console.error(`Missing .env variables: ${missingEnvVariables.join(', ')}`);
+        return true;
+    }
+    return false;
+}
 
 process.on('uncaughtException', (err, origin) => {
     console.error('Uncaught Exception:\n' +
@@ -77,20 +91,3 @@ process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection:\n' +
         'At:', promise, '\nReason:', reason);
 });
-
-const startup = async () => {
-    client.log('core', '');
-    try {
-        await import('@Plugins/index.js');
-        client.log('core', ':: Plugins Loaded');
-    } catch (err) {
-        console.error(err);
-        client.warn('core', 'Failed to load any plugins, a plugin has errors in it.');
-    }
-};
-
-export { client };
-
-startup();
-
-
